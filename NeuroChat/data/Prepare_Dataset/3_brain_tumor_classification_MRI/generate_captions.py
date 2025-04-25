0cd8ce38c7977012703da2e6983372e889ac3fa0
Ashmal Vayani <56957881+ashmalvayani@users.noreply.github.com>
2025-04-24 22:48:41 -0400

    Add files via upload

---
+++ NeuroChat/data/Prepare_Dataset/3_brain_tumor_classification_MRI/generate_captions.py
@@ -0,0 +1,132 @@
+import os
+import json
+import base64
+from PIL import Image
+from io import BytesIO
+import openai
+from tqdm import tqdm
+import pandas as pd
+
+from openai import AzureOpenAI
+
+endpoint = os.getenv("ENDPOINT_URL", "")  
+deployment = os.getenv("DEPLOYMENT_NAME", "gpt-4o-mini")
+subscription_key = os.getenv("AZURE_OPENAI_API_KEY", "")
+
+
+# Initialize Azure OpenAI Service client with key-based authentication    
+client = AzureOpenAI(  
+    azure_endpoint=endpoint,  
+    api_key=subscription_key,  
+    api_version="2024-10-21",
+)
+
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
+
+def generate_caption(labels_path, subset):
+    df = pd.read_csv(labels_path)
+
+    dl = []
+    failed = []
+    invalid_image_count = 0
+
+    batch = []
+
+    for index, row in tqdm(df.iterrows()):
+        image, tumor, tumor_type, image_type = row['image'], row['tumor'], row['tumor_type'], row['image_type']
+
+        image_path = "/home/parthpk/NeuroChat/Brain Tumor MRI"
+        if tumor == "no":
+            img_path = f"{image_path}/{subset}/no_tumor/{image}"
+        else:
+            img_path = f"{image_path}/{subset}/{tumor_type}_tumor/{image}"
+
+        base64_image = encode_image(img_path)
+
+        prompt_eval = f"Image metadata information:\n\n Tumor Presense: {tumor}, Tumor Type: {tumor_type}, Image view type: {image_type}"
+
+        system_prompt = "You are an expert brain neurologist. Given a brain MRI image and its view type (axial, coronal, or sagittal), analyze the scan and determine if a brain tumor is present. If a tumor is present, describe the tumor type, location, appearance, and its effect on surrounding brain tissue. If no tumor is present, clearly and confidently explain that the scan is normal and provide visual reasoning (e.g., preserved symmetry, absence of abnormal signals, or mass effect). \nWrite your response as a single, medically accurate paragraph in natural language. Do not include any section headers, bullet points, field names, markdown formatting, or metadata — only the descriptive paragraph focusing on clinical reasoning and visual findings."
+
+
+        PROMPT_MESSAGES = [
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text","text": prompt_eval},
+                    {"type": "image_url","image_url": {"url": f"data:image/jpeg;base64,{base64_image}"}}]
+            },
+            {"role": "system", "content": system_prompt}
+        ]
+
+        bat = {
+            "custom_id": f'train_{row["image"]}_{row["tumor_type"]}',
+            # "custom_id": f"{row['image']}_{row['tumor_type']}_{row['image_type']}",
+            "method": "POST", 
+            "url": "/chat/completions", 
+            "body": {
+                    "model": deployment,
+                    "messages": PROMPT_MESSAGES,
+                    "max_tokens": 1000,
+                    "temperature":0.7
+                }
+            }
+        
+        batch.append(bat)
+
+        # print(batch[-1])
+        # return 0
+
+
+    batc_output_path = "/home/ashmal/Courses/MedImgComputing/NeuroChat/Captions/3_brain_tumor_classification_MRI/TrainBatchFiles"
+    sub_batches = [batch[i:i + 1000] for i in range(0, len(batch), 1000)]
+    os.makedirs(batc_output_path, exist_ok=True)
+
+    for idx, sub_batch in enumerate(sub_batches):
+        with open(f'{batc_output_path}/3_json_batch_{idx}.jsonl', 'w') as f:
+            for entry in sub_batch:
+                f.write(json.dumps(entry))
+                f.write('\n')
+    
+    print(f"\n===========================\nDone making {len(sub_batches)} batches!")
+
+
+        # To check one by one response
+
+        # try:
+        #     response = client.chat.completions.create(
+        #         model = deployment,
+        #         messages = PROMPT_MESSAGES,
+        #         max_tokens = 1000,
+        #         temperature = 0.7
+        #     )
+
+        #     t_data = {
+        #         "id": row["no."],
+        #         "image": row["image"],
+        #         "tumor_type": row["tumor_type"],
+        #         "caption": response.choices[0].message.content,
+        #     }
+
+        #     dl.append(t_data)
+        #     print(dl[-1])
+        #     print()
+
+
+        # except Exception as e:
+        #     print(e)
+        #     failed.append(row["no."])
+        #     invalid_image_count += 1
+        #     pass
+
+        # if index == 5: 
+        #     exit()
+
+
+
+
+if __name__ == "__main__":
+    labels_path = "/home/parthpk/NeuroChat/Brain Tumor MRI/train_labels_o.csv"
+    subset = "Train"
+    generate_caption(labels_path, subset)
---
+++ NeuroChat/data/Prepare_Dataset/3_brain_tumor_classification_MRI/retrieve.py
@@ -0,0 +1,56 @@
+import json
+import os
+from openai import AzureOpenAI
+
+endpoint = os.getenv("ENDPOINT_URL", "")  
+deployment = os.getenv("DEPLOYMENT_NAME", "gpt-4o-mini")
+subscription_key = os.getenv("AZURE_OPENAI_API_KEY", "")
+
+# Initialize Azure OpenAI Service client with key-based authentication    
+client = AzureOpenAI(  
+    azure_endpoint=endpoint,  
+    api_key=subscription_key,
+    api_version="2024-08-01-preview",
+)
+
+with open('train_batch_ids.json', 'r') as file:
+    data = json.load(file)
+
+batch_ids = [d['batch_id'] for d in data]
+
+
+for batch_id in batch_ids:
+    batch_response = client.batches.retrieve(batch_id)
+
+    output_file_id = batch_response.output_file_id
+
+    if not output_file_id:
+        output_file_id = batch_response.error_file_id
+
+    if output_file_id:
+        # loading ground truths from the input file
+        file_response = client.files.content(output_file_id)
+        raw_responses = file_response.text.strip().split('\n')  
+
+        final_outputs = []
+        for index, raw_response in enumerate(raw_responses):
+            json_response = dict(json.loads(raw_response))
+            custom_id = json_response["custom_id"]
+
+            try:
+                caption = json_response["response"]["body"]["choices"][0]["message"]["content"]
+            except:
+                print(f"unable to fetch caption for {batch_id}, {index}.")
+                continue
+
+            final_outputs.append(
+                {
+                    "image_id": custom_id,
+                    "caption": caption
+                }
+            )
+
+        print(f"\n====================\n{batch_id} fetched total samples: {len(final_outputs)}\n")
+
+        with open(f"Outputs/Train/{batch_id}.json", "w") as f:
+            json.dump(final_outputs, f, indent=2)
